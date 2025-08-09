cedea9640a99c2803a09938c7d45fb43698f8ebb
USER <tahsinahmed.iit@gmail.com>
2025-08-10 02:02:01 +0600

    Recurring Transaction summary api is integrated

---
+++ src/main/java/com/project/Expense/Tracker/Entity/DTO/RecurringTransactionSummaryDTO.java
@@ -0,0 +1,16 @@
+package com.project.Expense.Tracker.Entity.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecurringTransactionSummaryDTO {
+    private BigDecimal totalIncome;
+    private BigDecimal totalExpense;
+}
+
